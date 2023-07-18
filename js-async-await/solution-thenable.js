03a5f90522e573ea030ae7926bc4a04692325ed8
sagarzemosolabs <sagar.yadav@zemosolabs.com>
2023-07-18 19:33:51 +0530

    completed js async await assignment

---
+++ js-async-await/solution-thenable.js
@@ -0,0 +1,21 @@
+
+
+function getData(uId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        console.log("Fetched the data!");
+        resolve("skc@gmail.com");
+        }, 4000);
+    });
+}
+
+async function print() {
+    console.log("start");
+    getData("skc").then((email) => {
+        console.log("Email id of the user id is: " + email);
+    console.log("end");
+    });
+    
+}
+
+print();
